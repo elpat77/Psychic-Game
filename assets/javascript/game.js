ac799d47d51d6ab022f92e9ebf66e7bca9daae9d
elpat7 <aguilarpat7@gmail.com>
2019-09-29 17:29:12 -0700

    working wins/loss count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,17 +24,28 @@ console.log(losses);
 
 var myLetters = ['p', 'a', 't'];
 
+function updateWins() {
+    document.querySelector("#wins").innerHTML = "Wins: " + score;
+}
+function updateLosses() {
+    document.querySelector("#losses").innerHTML = "Losses: " + losses;
+}
+updateWins();
+updateLosses();
+
 document.onkeyup = function (event) {
     console.log(event.key);
     document.querySelector('#char').textContent = event.key;
     console.log(myLetters)
     if (event.key === myLetters[0] || event.key === myLetters[1] || event.key === myLetters[2]) {
         score++;
+        updateWins();
         console.log("You got it!");
         console.log(score);
         console.log(losses);
     } else {
         losses++;
+        updateLosses();
         console.log("Try again!");
         console.log(score);
         console.log(losses);
