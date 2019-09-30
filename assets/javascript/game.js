24277edf4cd65ac9d2168459211a88410e425da9
elpat7 <aguilarpat7@gmail.com>
2019-09-29 22:54:56 -0700

    updating console logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
+console.log("Starting new game");
+
 var wins = 0;
 var losses = 0;
 var remaining = 9;
 var guesses = [];
-
 
 var myLetters = ['p', 'a', 't'];
 
@@ -26,38 +27,40 @@ updateRemaining();
 updateGuessed();
 
 document.onkeyup = function (event) {
-    document.querySelector("#selected").textContent = event.key;
+    // document.querySelector("#selected").textContent = event.key;
 
     if (event.key === myLetters[0] || event.key === myLetters[1] || event.key === myLetters[2]) {
         wins++;
         updateWins();
-        alert("You got it!");
+        alert("Congratulations, you got it!");
         remaining = 9;
         document.querySelector("#remaining").innerHTML = "Remaining guesses: " + remaining;
         guesses = [];
         document.querySelector("#guessed").innerHTML = "Guessed so Far: " + guesses;
-        console.log(wins);
-        console.log(losses);
+        console.log("You guessed the letter!");
+        console.log("Wins: " + wins);
+        console.log("Starting new game");
+
     } else {
         remaining--;
         guesses.push(event.key);
         updateRemaining();
         updateGuessed();
-        console.log("Try again!");
-        console.log(wins);
-        console.log(losses);
-        console.log(remaining);
-        console.log(guesses);
+        console.log("Wrong Letter, try again!");
+        console.log("Remaining guesses: " + remaining);
+        console.log("Guessed so Far: " + guesses);
     }
 
     if (remaining === 0) {
         losses++;
         updateLosses();
         alert("Sorry you lost, try again!")
+        console.log("Game Over!");
         remaining = 9;
         document.querySelector("#remaining").innerHTML = "Remaining guesses: " + remaining;
         guesses = [];
         document.querySelector("#guessed").innerHTML = "Guessed so Far: " + guesses;
+        console.log("Starting new game");
     }
 }
 
