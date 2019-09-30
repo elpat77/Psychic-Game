1b9c2f9b38c460defecc1a111d2fc18ed5c3ba1a
elpat7 <aguilarpat7@gmail.com>
2019-09-29 17:48:02 -0700

    working updateLosses count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,42 @@
-var score = 0;
-console.log(score);
+var wins = 0;
 var losses = 0;
-console.log(losses);
-
-//     wins: '',
-//     losses: '',
-//     guessesLeft: '9',
-//     guessesSoFar: '',
-// }
-
-// var score = 0;
-
-// var losses = 0;
-// var guessesLeft = 9;
-// var guessesSoFar = 0;
-
-// wins: function () {
-//     alert('Wins: ' + this.wins);
-//     this.wins = this.wins + 1;
-//     alert('New wins: ' + this.wins);
-// }
-// };
+var left = 9;
 
 var myLetters = ['p', 'a', 't'];
 
 function updateWins() {
-    document.querySelector("#wins").innerHTML = "Wins: " + score;
+    document.querySelector("#wins").innerHTML = "Wins: " + wins;
 }
 function updateLosses() {
     document.querySelector("#losses").innerHTML = "Losses: " + losses;
 }
+function updateLeft() {
+    document.querySelector("#left").innerHTML = "Left: " + left;
+}
+
 updateWins();
 updateLosses();
+updateLeft();
 
 document.onkeyup = function (event) {
     console.log(event.key);
     document.querySelector('#char').textContent = event.key;
     console.log(myLetters)
     if (event.key === myLetters[0] || event.key === myLetters[1] || event.key === myLetters[2]) {
-        score++;
+        wins++;
         updateWins();
         console.log("You got it!");
-        console.log(score);
+        console.log(wins);
         console.log(losses);
     } else {
         losses++;
+        left--;
         updateLosses();
+        updateLeft();
         console.log("Try again!");
-        console.log(score);
+        console.log(wins);
         console.log(losses);
+        console.log(left);
 
     }
 }
